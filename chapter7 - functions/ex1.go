964a43d8a92801ece256b95fb600abf2ddcc742b
Mnemonic Flow <mnemonicflow@gmail.com>
2015-02-23 21:32:41 +0200

    chapter7 - exercise 1

---
+++ chapter7 - functions/ex1.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+ sum is a function which takes a slice of numbers
+and adds them together. What would its function signature look like in Go?
+*/
+
+import (
+  "fmt"
+  //"reflect"
+)
+
+func sum(slice []int) int {
+  total := 0
+  
+  for _, val := range slice {
+    total += val
+  }
+  
+  return total
+}
+
+func main() {
+  fmt.Println(sum([]int {1, 2, 3}))
+}
