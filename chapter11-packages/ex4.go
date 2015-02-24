007082680f71708ca97a62cfd01ae2664d6aea51
Mnemonic Flow <mnemonicflow@gmail.com>
2015-02-25 00:10:15 +0200

    chapter11 - exercise 4

---
+++ chapter11-packages/ex4.go
@@ -0,0 +1,12 @@
+package main 
+
+import "fmt"
+import m "an-introduction-to-programming-in-go-exercises/chapter11-packages/math" 
+
+func main() {
+  xs := []float64 { 1, 2, 3, 4 }
+  avg := m.Average(xs)
+  fmt.Println(avg, m.Min(xs), m.Max(xs))
+}
+
+// godoc an-introduction-to-programming-in-go-exercises/chapter11-packages/math
